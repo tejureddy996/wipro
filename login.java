2a02a15b78b68a85ec6ee103c5cf3e05f1b656ea
root <root@ip-172-31-18-85.ec2.internal>
2022-10-04 14:54:29 +0000

    this is details about login and logout files

---
+++ login.java
@@ -0,0 +1,7 @@
+<html>
+<head> this is sample code</head>
+<body>
+<h1>login file </h1>
+</body>
+</html>
+
---
+++ logout.java
@@ -0,0 +1,6 @@
+<html>
+<head> this is java file</head>
+<body>
+<h1> this is logout file </h1>
+</body>
+</html>
